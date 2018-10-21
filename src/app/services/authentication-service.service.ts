78426f895a2ee23cc599ffe641a3d0ebb9378024
Abderrhim Soubai <gx.coder@gmail.com>
2018-10-21 02:40:10 +0100

    Forget to save

--- src/app/services/authentication-service.service.ts
+++ src/app/services/authentication-service.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthenticationServiceService {
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    constructor(private http: HttpClient) { }
 
-  constructor() { }
+    login(username: string, password: string) {
+        return this.http.post<any>(`${environment.apiUrl}/login`, { username, password })
+            .pipe(map(user => {
+                if (user && user.token) {
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
+
+                return user;
+            }));
+    }
+
+    getCurrentUser(){
+
+      return Observable.create(observer =>{
+
+          observer.next(localStorage.getItem('currentUser'));
+
+      });
+
+      
+
+    }
+
+    logout() {
+        localStorage.removeItem('currentUser');
+    }
 }
--- src/app/services/foods.service.ts
+++ src/app/services/foods.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FoodsService {
+export class FoodsService   {
 
-  constructor() { }
+  constructor(private api:ApiService){
+
+    this.api.resource = 'foods';
+
+  }
+
+
+  getFoods(){
+    return this.api.getAll();
+  }
+
+  getFoodById(id:any){
+    return this.api.getById(id);
+  }
+
+
+
+
 }
